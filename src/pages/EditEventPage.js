0f1f06d470e4ec0cd5579334930f27ec72317684
Engi Lozada <engilozada@gmail.com>
2023-05-22 20:29:40 +0200

    Create pages components

---
+++ src/pages/EditEventPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditEventPage = () => {
+  return <div>EditEventPage</div>;
+};
+
+export default EditEventPage;
---
+++ src/pages/NewEventPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewEventPage = () => {
+  return <div>NewEventPage</div>;
+};
+
+export default NewEventPage;
