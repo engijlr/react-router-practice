0dc564133203992d03a1e1357e05cc38e357f145
Engi Lozada <engilozada@gmail.com>
2023-05-23 20:01:48 +0200

    fe
    git commit -m fe

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,22 +1,11 @@
-import { Link } from "react-router-dom";
-const DUMMY_DATA = [
-  { id: "e1", title: "One Event" },
-  { id: "e2", title: "Another Event" },
-];
+import { useLoaderData } from "react-router-dom";
 
-const EventsPage = () => {
-  return (
-    <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_DATA.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-};
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+  const events = useLoaderData();
+
+  return <EventsList events={events} />;
+}
 
 export default EventsPage;
